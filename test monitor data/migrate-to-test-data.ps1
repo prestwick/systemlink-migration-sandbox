b8115473bd9d7a452a0e82167f376eaaa5f33eff
Mark Black <mark.prestwick@gmail.com>
2020-07-21 16:57:16 -0500

    basics of migration

---
+++ test monitor data/migrate-to-test-data.ps1
@@ -0,0 +1,18 @@
+# migrate-to-test-data.ps1
+
+# This example will migrate all test results generated from running migrate-from-test-data.ps1 to another SystemLink Server
+# This script will not migrate file attached to test results 
+
+# Set variables for various paths used during migration
+$MigrationDir = "C:\migration"
+$NoSqlDumpDir = Join-Path $MigrationDir mongo-dump
+$MongoRestore = Join-Path $env:ProgramFiles "National Instruments" Shared Skyline NoSqlDatabase bin mongorestore.exe
+# $SlConfCmd = Join-Path $env:ProgramFiles "National Instruments" Shared Skyline NISystemLinkServerConfigCmd.exe
+# $SlDataDir = Join-Path $env:ProgramData "National Instruments Skyline Data"
+
+# Import MongoDB dump files 
+$Service = "TestMonitor"
+$ConfigFile = Join-Path $env:ProgramData "National Instruments" Skyline Config "$Service.json"
+$Config = (Get-Content $ConfigFile | ConvertFrom-Json).$Service
+$DumpFile = Join-Path $NoSqlDumpDir $Config."Mongo.Database"
+. $MongoRestore --port $Config."Mongo.Port" --db $Config."Mongo.Database" --username $Config."Mongo.User" --password $Config."Mongo.Password" --gzip $DumpFile
