cbe28efc2df8cb9aed2cfab024cd422d3fa59d52
Mark Black <mark.prestwick@gmail.com>
2020-07-22 11:20:57 -0500

    removed unneeded slash

--- test monitor data/migrate-from-test-data.py
+++ test monitor data/migrate-from-test-data.py
@@ -6,5 +6,5 @@ print(s)
 migration_directory = 'C:\migration'
 no_sql_dump_dir = os.path.join(migration_directory, 'mongo-dump')
 program_file_dir = os.environ.get("PROGRAMFILES")
-mongo_dump = os.path.join(program_file_dir, "National\ Instruments", "Shared/Skyline/NoSqlDatabase/bin")
+mongo_dump = os.path.join(program_file_dir, "National Instruments", "Shared/Skyline/NoSqlDatabase/bin")
 print(mongo_dump)
