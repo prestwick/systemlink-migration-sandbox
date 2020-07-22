8c6f4c692becdba627d968138c509566e620748c
Mark Black <mark.prestwick@gmail.com>
2020-07-22 15:35:36 -0500

    successfully migrating from

--- file ingestion/migrate-from-fis.py
+++ file ingestion/migrate-from-fis.py
@@ -8,14 +8,14 @@ import os, subprocess, json, shutil
 migration_dir = "C:\migration"
 no_sql_dump_dir = os.path.join(migration_dir, "mongo-dump")
 program_file_dir = os.environ.get("ProgramW6432")
-prorgram_data_dir = os.environ.get("ProgramData")
-fis_data_source_dir = os.path.join(program_file_dir, "National Instruments", "Shared", "Skyline", "Data", "FileIngestion")
+program_data_dir = os.environ.get("ProgramData")
+fis_data_source_dir = os.path.join(program_data_dir, "National Instruments", "Skyline", "Data", "FileIngestion")
 fis_data_migration_dir = os.path.join(migration_dir, "FileIngestion")
 mongo_dump = os.path.join(program_file_dir, "National Instruments", "Shared", "Skyline", "NoSqlDatabase", "bin", "mongodump.exe")
 
 # Get data from service's json config file 
 service= "FileIngestion"
-config_file = os.path.join(prorgram_data_dir, "National Instruments", "Skyline", "Config", service+".json")
+config_file = os.path.join(program_data_dir, "National Instruments", "Skyline", "Config", service+".json")
 with open(config_file, encoding='utf-8-sig') as json_file:
     config = json.load(json_file)
 
@@ -24,6 +24,7 @@ mongo_dump_cmd = mongo_dump + " --port " + str(config[service]['Mongo.Port']) + 
 subprocess.run(mongo_dump_cmd)
 
 # Copy ingested files to migration direction 
+os.mkdir(fis_data_migration_dir)
 migration_files = os.listdir(fis_data_source_dir)
 for file_name in migration_files:
     full_file_path = os.path.join(fis_data_source_dir, file_name)
--- test monitor data/migrate-from-test-data.py
+++ test monitor data/migrate-from-test-data.py
@@ -9,12 +9,12 @@ import os, subprocess, json
 migration_directory = "C:\migration"
 no_sql_dump_dir = os.path.join(migration_directory, "mongo-dump")
 program_file_dir = os.environ.get("ProgramW6432")
-prorgram_data_dir = os.environ.get("ProgramData")
+program_data_dir = os.environ.get("ProgramData")
 mongo_dump = os.path.join(program_file_dir, "National Instruments", "Shared", "Skyline", "NoSqlDatabase", "bin", "mongodump.exe")
 
 # Get data from service's json config file 
 service= "TestMonitor"
-config_file = os.path.join(prorgram_data_dir, "National Instruments", "Skyline", "Config", service+".json")
+config_file = os.path.join(program_data_dir, "National Instruments", "Skyline", "Config", service+".json")
 with open(config_file, encoding='utf-8-sig') as json_file:
     config = json.load(json_file)
 
