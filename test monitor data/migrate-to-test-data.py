06d5e11b8b46d0cb4576dac7bb6d403964dd155d
Mark Black <mark.prestwick@gmail.com>
2020-07-22 14:03:38 -0500

    Create migrate-to-test-data.py

---
+++ test monitor data/migrate-to-test-data.py
@@ -0,0 +1,19 @@
+import os, subprocess, json
+
+migration_directory = "C:\migration"
+no_sql_dump_dir = os.path.join(migration_directory, "mongo-dump")
+
+program_file_dir = os.environ.get("ProgramW6432")
+prorgram_data_dir = os.environ.get("ProgramData")
+mongo_restore = os.path.join(program_file_dir, "National Instruments", "Shared", "Skyline", "NoSqlDatabase", "bin", "mongorestore.exe")
+
+service = "TestMonitor"
+config_file = os.path.join(prorgram_data_dir, "National Instruments", "Skyline", "Config", service+".json")
+
+with open(config_file, encoding='utf-8-sig') as json_file:
+    config = json.load(json_file)
+
+mongo_dump_file = os.path.join(no_sql_dump_dir, config[service]['Mongo.Databases'])
+
+mongo_restore_cmd = mongo_dump + " --port " + str(config[service]['Mongo.Port']) + " --db " + config[service]['Mongo.Database'] + " --username " + config[service]['Mongo.User'] + " --password " + config[service]['Mongo.Password'] + " --gzip " + mongo_dump_file
+subprocess.run(mongo_restore_cmd)
