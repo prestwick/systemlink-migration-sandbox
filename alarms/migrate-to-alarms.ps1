3310cf46285ebebc505e6246a815915092de1b45
Mark Black <mark.prestwick@gmail.com>
2020-07-22 08:07:31 -0500

    first pass alarm to/from

--- alarms/migrate-to-alarms.ps1
+++ alarms/migrate-to-alarms.ps1
@@ -1,7 +1,7 @@
-# migrate-to-test-data.ps1
+# migrate-to-alarms.ps1
 
-# This example will migrate all test results generated from running migrate-from-test-data.ps1 to another SystemLink Server
-# This script will not migrate file attached to test results 
+# This example will migrate all tag alarm rules generated from running migrate-from-alarms.ps1 to another SystemLink Server
+# This script does not migrate tags associated with alarms, or  the alarm instances generated from these rule. 
 
 # Set variables for various paths used during migration
 $MigrationDir = "C:\migration"
@@ -9,7 +9,7 @@ $NoSqlDumpDir = Join-Path $MigrationDir mongo-dump
 $MongoRestore = Join-Path $env:ProgramFiles "National Instruments" Shared Skyline NoSqlDatabase bin mongorestore.exe
 
 # Import MongoDB dump files 
-$Service = "TestMonitor"
+$Service = "TagRuleEngine"
 $ConfigFile = Join-Path $env:ProgramData "National Instruments" Skyline Config "$Service.json"
 $Config = (Get-Content $ConfigFile | ConvertFrom-Json).$Service
 $DumpFile = Join-Path $NoSqlDumpDir $Config."Mongo.Database"
