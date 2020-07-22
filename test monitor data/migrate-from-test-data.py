d1da2e5832bbd8c6eec4da4b70bdb534cf7fd577
Mark Black <mark.prestwick@gmail.com>
2020-07-22 11:24:41 -0500

    attempt calling mongodump

--- test monitor data/migrate-from-test-data.py
+++ test monitor data/migrate-from-test-data.py
@@ -3,8 +3,11 @@ import os
 s = "Hello, world"
 print(s)
 
-migration_directory = 'C:\migration'
-no_sql_dump_dir = os.path.join(migration_directory, 'mongo-dump')
+migration_directory = "C:\migration"
+no_sql_dump_dir = os.path.join(migration_directory, "mongo-dump")
 program_file_dir = os.environ.get("PROGRAMFILES")
-mongo_dump = os.path.join(program_file_dir, "National Instruments", "Shared", "Skyline" "NoSqlDatabase", "bin")
+mongo_dump = os.path.join(program_file_dir, "National Instruments", "Shared", "Skyline" "NoSqlDatabase", "bin", "mongodump.exe")
 print(mongo_dump)
+
+service= "TestMonitor"
+os.system(mongo_dump)
