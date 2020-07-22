d0fa5c8aac4c3ea560b3b203bb016331e98bf9ae
Mark Black <mark.prestwick@gmail.com>
2020-07-22 14:16:30 -0500

    misspelling

--- test monitor data/migrate-to-test-data.py
+++ test monitor data/migrate-to-test-data.py
@@ -13,7 +13,7 @@ config_file = os.path.join(prorgram_data_dir, "National Instruments", "Skyline",
 with open(config_file, encoding='utf-8-sig') as json_file:
     config = json.load(json_file)
 
-mongo_dump_file = os.path.join(no_sql_dump_dir, config[service]['Mongo.Databases'])
+mongo_dump_file = os.path.join(no_sql_dump_dir, config[service]['Mongo.Database'])
 
 mongo_restore_cmd = mongo_restore + " --port " + str(config[service]['Mongo.Port']) + " --db " + config[service]['Mongo.Database'] + " --username " + config[service]['Mongo.User'] + " --password " + config[service]['Mongo.Password'] + " --gzip " + mongo_dump_file
 subprocess.run(mongo_restore_cmd)
