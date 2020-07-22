9f20f57d085ea454a932df4a75d076c06384b230
Mark Black <mark.prestwick@gmail.com>
2020-07-22 11:18:21 -0500

    Create migrate-from-test-data.py

---
+++ test monitor data/migrate-from-test-data.py
@@ -0,0 +1,10 @@
+import os
+
+s = "Hello, world"
+print(s)
+
+migration_directory = 'C:\migration'
+no_sql_dump_dir = os.path.join(migration_directory, 'mongo-dump')
+program_file_dir = os.environ.get("PROGRAMFILES")
+mongo_dump = os.path.join(program_file_dir, "National\ Instruments", "Shared/Skyline/NoSqlDatabase/bin")
+print(mongo_dump)
