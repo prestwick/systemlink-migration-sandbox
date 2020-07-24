67370ddc1f4031d8fe10676bf4f324603226d28d
Mark Black <mark.prestwick@gmail.com>
2020-07-24 16:39:15 -0500

    can't get both app to run and test to pass
    Weirdness with imports. I can eithe set up the imports such that the app will run or the test will pass, but not both.

--- slmigrate/systemlinkmigrate.py
+++ slmigrate/systemlinkmigrate.py
@@ -2,6 +2,18 @@
 # Not all services will be supported. Addtional services will be supported over time. 
 
 import os, json, shutil, subprocess, argparse, sys
+# from slmigrate.migrate import restore
+# from slmigrate.migrate import capture
+# import migrate
+# from slmigrate.migrate import restore
+
+# his will makes test work, but the script will fail. 
+# from slmigrate.migrate import restore
+# from slmigrate.migrate import capture
+
+# This will make the app work but test will fail. 
+from migrate import restore
+from migrate import capture
 
 # Global Constants
 migration_dir = os.path.join(os.path.abspath(os.sep), "migration")
@@ -16,11 +28,11 @@ no_sql_dump_dir = os.path.join(migration_dir, "mongo-dump")
 tagservice = "TagIngestion"
 opcservice = "OpcClient"
 
-
-
 # Setup available command line arguments
 def parse_arguments(args):
     parser = argparse.ArgumentParser()
+    parser.add_argument ("--capture", help="capture is used to pull data and settings off SystemLink server", action="store_true", )
+    parser.add_argument ("--restore", help="restore is used to push data and settings to a clean SystemLink server. ", action="store_true", )
     parser.add_argument ("--tag", "--tags", "--tagingestion", "--taghistory", help="Migrate tags and tag histories", action="store_true", )
     parser.add_argument ("--opc", "--opcua", "--opcuaclient", help="Migrate OPCUA sessions and certificates", action="store_true")
     return  parser 
@@ -29,17 +41,23 @@ def add_numbers(num1, num2):
     sum = num1 + num2
     return sum
 
-def migrate_tags():
-    print("migrate_tags called")
-
-def migrate_opc():
-    print("migrate_opc called")
-
-
 # Main
 if __name__ == "__main__":
     arguments = parse_arguments(sys.argv[1:]).parse_args()
+    if not(arguments.capture) and not(arguments.restore):
+        print("Please use --capture or --restore to determine which direction the migration is occuring. ")
+    if arguments.capture and arguments.restore:
+        print("You cannot use --capture and --restore simultaneously. ")
     if arguments.tag:
-        migrate_tags()
+        if arguments.capture:
+            capture.capture_migration(tagservice)
+        if arguments.restore:
+            restore.restore_migration(tagservice)
     if arguments.opc:
-        migrate_opc()
+        if arguments.capture:
+            capture.capture_migration(opcservice)
+        if arguments.restore:
+            restore.restore_migration(opcservice)
+
+
+        
--- test/test_slmigrate.py
+++ test/test_slmigrate.py
@@ -1,16 +1,16 @@
 import pytest
-from slmigrate import slmigrate
-
+# from slmigrate import slmigrate
+# from slmigrate import systemlinkmigrate
+from slmigrate import systemlinkmigrate
 
 def test_main():
     pass
 
 def test_add_numbers():
-    sum = slmigrate.add_numbers(1,2)
+    sum = systemlinkmigrate.add_numbers(1,2)
     assert sum == 3
 
 def test_parse_arguments():
-    parser = slmigrate.parse_arguments(["--tags", "--tag", "--taghistory", "--tagingestion", "--opc", "--opcua", "--opcuaclient"])
-    # assert parser.parse_known_args(["Namespace", ["--tags"]])
+    parser = systemlinkmigrate.parse_arguments(["--tags", "--tag", "--taghistory", "--tagingestion", "--opc", "--opcua", "--opcuaclient"])
     assert parser.parse_known_args()
 
