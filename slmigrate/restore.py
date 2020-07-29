9d3df72ff8e6e4776ef88b6dfae46f11e6cd96f4
Mark Black <mark.prestwick@gmail.com>
2020-07-29 09:07:17 -0500

    fixed variable

--- slmigrate/restore.py
+++ slmigrate/restore.py
@@ -5,7 +5,7 @@ def restore_mongo_data(service):
     config_file = os.path.join(constants.program_data_dir, "National Instruments", "Skyline", "Config", service.name +".json")
     with open(config_file, encoding='utf-8-sig') as json_file:
         config = json.load(json_file)
-        mongo_dump_file = os.path.join(constants.no_sql_dump_dir, config[service]['Mongo.Database'])
+        mongo_dump_file = os.path.join(constants.no_sql_dump_dir, config[service.name]['Mongo.Database'])
         subprocess.run(constants.mongo_restore + " --port " + str(config[service.name]['Mongo.Port']) + " --db " + config[service.name]['Mongo.Database'] + " --username " + config[service.name]['Mongo.User'] + " --password " + config[service.name]['Mongo.Password'] + " --gzip " + mongo_dump_file)
 
 
