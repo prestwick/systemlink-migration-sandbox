2c5e5558aab167f7c2946483432a5a544a60e0cf
Mark Black <mark.prestwick@gmail.com>
2020-07-29 17:45:37 -0500

    incorrect is else

--- slmigrate/common.py
+++ slmigrate/common.py
@@ -2,6 +2,7 @@ from slmigrate import constants
 from distutils import dir_util
 import json, subprocess, os, sys, shutil
 
+#something is wrong with both capture and restore
 def migrate_mongo_cmd(service, action):
     config_file = os.path.join(constants.program_data_dir, "National Instruments", "Skyline", "Config", service.name +".json")
     with open(config_file, encoding='utf-8-sig') as json_file:
@@ -9,8 +10,11 @@ def migrate_mongo_cmd(service, action):
     mongo_dump_file = os.path.join(constants.no_sql_dump_dir, config[service.name]['Mongo.Database'])
     if action == constants.capture_arg:
         cmd_to_run = constants.mongo_dump + " --port " + str(config[service.name]['Mongo.Port']) + " --db " + config[service.name]['Mongo.Database'] + " --username " + config[service.name]['Mongo.User'] + " --password " + config[service.name]['Mongo.Password'] + " --out " + constants.no_sql_dump_dir + " --gzip"
-    elif action:
+    elif action == constants.restore_arg:
         cmd_to_run = constants.mongo_restore + " --port " + str(config[service.name]['Mongo.Port']) + " --db " + config[service.name]['Mongo.Database'] + " --username " + config[service.name]['Mongo.User'] + " --password " + config[service.name]['Mongo.Password'] + " --gzip " + mongo_dump_file
+    subprocess.run(constants.mongo_restore + " --port " + str(config[service.name]['Mongo.Port']) + " --db " + config[service.name]['Mongo.Database'] + " --username " + config[service.name]['Mongo.User'] + " --password " + config[service.name]['Mongo.Password'] + " --gzip " + mongo_dump_file)
+
+    
     subprocess.run(cmd_to_run)
 
 def stop_sl_service(service):
