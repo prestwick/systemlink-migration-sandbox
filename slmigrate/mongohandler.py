d45b4f8809c5bf6569ad81dabaed9b1d82ebcee7
Mark Black <mark.prestwick@gmail.com>
2020-08-25 10:48:00 -0500

    algorithm to merge histories

--- slmigrate/mongohandler.py
+++ slmigrate/mongohandler.py
@@ -5,6 +5,7 @@ import os
 from pymongo import MongoClient
 from pymongo import errors as pyerr
 import bson
+from types import SimpleNamespace
 
 
 def get_service_config(service):
@@ -37,12 +38,65 @@ def restore_migration(service, action, config):
     subprocess.run(cmd_to_run)
 
 
-def merge_metadata():
-    pass
+def migrate_document(destination_collection, document):
+    try:
+        print("Migrating " + str(document['_id']))
+        destination_collection.insert_one(document)
+    except pyerr.DuplicateKeyError:
+        print("Docuement " + str(document['_id']) + " already exists. Skipping")
 
 
-def merge_values():
-    pass
+def identify_metadata_conflict(destination_collection, source_document):
+    # source_query = {'$and': [{'metadataId': document['_id']}, {'$and': [{'workspace': document['workspace']}, {'path': document['path']}]}]}
+
+    # use projections for source document to determine fields to match in destination document. save off both of their _ids
+    # source_query = {'_id': document['_id']}
+    # source_projection = {'$elematch': {'workspace', 'path'}}
+
+    destination_query = {'$and': [{'workspace': source_document['workspace']}, {'path': source_document['path']}]}
+    # source_document = source_collection.find_one(source_query)
+    destination_document = destination_collection.find_one(destination_query)
+
+    # if source_collection.find_one(source_query) == destination_collection.find_one(destination_query):
+    # if source_document == destination_document:
+    # field_value = document['_id']
+    # return SimpleNamespace(**{'destination_document': destination_document, 'field_value': field_value})
+
+    if destination_document:
+        return SimpleNamespace(**{'source_id': source_document['_id'], 'destination_id': destination_document['_id']})
+    else:
+        return None
+
+
+def merge_history_document(source_id, destination_id, destination_db):
+    destination_collection = destination_db.get_collection('values')
+    destination_collection.update_one({'metadataId': source_id}, {'metadataId': destination_id})
+
+
+def migrate_metadata_collection(source_db, destination_db):
+    collection_name = 'metadata'
+    # check if workspace and paths match, don't migrate, but cache id
+    # if they dont match try and move the document to destination db
+    source_collection = source_db.get_collection(collection_name)
+    source_collection_iterable = source_collection.find()
+    destination_collection = destination_db.get_collection(collection_name)
+
+    # if collection_name == 'metatdata':
+    for document in source_collection_iterable:
+        conflict = identify_metadata_conflict(destination_collection, document)
+        if conflict:
+            merge_history_document(conflict.source_id, conflict.destination_id, destination_db)
+        else:
+            migrate_document(destination_collection, document)
+
+
+def migrate_values_collection(source_db, destination_db):
+    collection_name = 'values'
+    collection_iterable = source_db.get_collection(collection_name).find()
+    destination_collection = destination_db.get_collection(collection_name)
+
+    for document in collection_iterable:
+        migrate_document(destination_collection, document)
 
 
 def migrate_within_instance(service, action, config):
@@ -51,18 +105,10 @@ def migrate_within_instance(service, action, config):
     codec = bson.codec_options.CodecOptions(uuid_representation=bson.binary.UUID_SUBTYPE)
     no_sql_config = get_service_config(constants.no_sql)
     client = MongoClient(host=[no_sql_config[constants.no_sql.name]['Mongo.Host']], port=no_sql_config[constants.no_sql.name]['Mongo.Port'], username=no_sql_config[constants.no_sql.name]['Mongo.User'], password=no_sql_config[constants.no_sql.name]['Mongo.Password'])
-    source_db = client.get_database(name=constants.source_db, codec_options=codec)
+    source_db = client.get_database(name=service.source_db, codec_options=codec)
     destination_db = client.get_database(name=service.destination_db, codec_options=codec)
-
-    for collection in service.collections_to_migrate:
-        source_collection = source_db.get_collection(collection).find()
-        destination_collection = destination_db.get_collection(collection)
-        for document in source_collection:
-            try:
-                print("Migrating " + str(document['_id']))
-                destination_collection.insert_one(document)
-            except pyerr.DuplicateKeyError:
-                print("Docuement " + str(document['_id']) + " already exists. Skipping")
+    migrate_values_collection(source_db, destination_db)
+    migrate_metadata_collection(source_db, destination_db)
 
 
 def migrate_mongo_cmd(service, action, config):
