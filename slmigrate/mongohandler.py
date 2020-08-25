66dedee80a1314666af34aa8cd54997cd269215f
Mark Black <mark.prestwick@gmail.com>
2020-08-25 11:24:04 -0500

    more debug strings

--- slmigrate/mongohandler.py
+++ slmigrate/mongohandler.py
@@ -89,6 +89,7 @@ def migrate_metadata_collection(source_db, destination_db):
             print("Conflict Found! " + "source_id=" + str(conflict.source_id) + " destination_id=" + str(conflict.destination_id))
             merge_history_document(conflict.source_id, conflict.destination_id, destination_db)
         else:
+            print('Migrating metadata document=' + str(document['_id'])
             migrate_document(destination_collection, document)
 
 
