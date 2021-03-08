ed09cfdeb05b73cf1e0491f5fceacd2e774524d1
michaeldbrandt <michael.brandt@ni.com>
2021-03-08 15:38:43 -0600

    Got Python 3.7 working after some issues with my dev machine (conflicting versions)

--- test/context.py
+++ test/context.py
@@ -2,4 +2,5 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# This is a workaround due to project structure.  TODO: replace tests that reference main() with a more sustainable model.
 import systemlinkmigrate
